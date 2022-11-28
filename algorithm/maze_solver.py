95ce3ea3cac7f704610e082ceacb8cca246ce45e
scottshanfeld <74272645+scottshanfeld@users.noreply.github.com>
2022-11-28 18:14:37 -0500

    Update maze_solver.py

--- algorithm/maze_solver.py
+++ algorithm/maze_solver.py
@@ -1,5 +1,5 @@
 from maze_path import MazePath
-from image_processor import get_image, has_reached_end, has_straight_path, has_right_path, has_left_path
+from image_processor import get_image, has_reached_end, has_straight_path, has_right_path, has_left_path, process_image
 
 URL = 'http://x.x.x.x/caputre'
 
@@ -8,21 +8,22 @@ def solve_maze():
 
     while (True):
         image = get_image(URL)
+        process_image(image)
 
-        if not has_reached_end(image):
+        if not has_reached_end():
             # Move forward an inch
-            if has_left_path(image): # Can bot go left
+            if has_left_path(): # Can bot go left
                 # Rotate -90 degrees
                 maze_path.push_move('R')
                 pass
-            elif has_straight_path(image) and has_right_path():
+            elif has_straight_path() and has_right_path():
                 # Stay straight
                 maze_path.push_move('S')
                 pass
-            elif has_straight_path(image):
+            elif has_straight_path():
                 # Stay straight
                 pass
-            elif has_right_path(image):
+            elif has_right_path():
                 # Rotate 90 degrees
                 maze_path.push_move('L')
                 pass
